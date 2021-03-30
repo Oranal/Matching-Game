9cc236d7e14f2610a7a82688965aefd8e7fb856e
BarAzenkot <barazenkot3@gmail.com>
2021-03-30 15:48:49 +0300

    Accounts DB

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Account(models.Model):
+    first_name = models.CharField(max_length=40)
+    last_name = models.CharField(max_length=40)
+    username = models.CharField(max_length=40, unique=True)
+    password = models.CharField(max_length=40)    
+    rating = models.IntegerField()
+    institution = models.CharField(max_length=60)
+    role = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.first_name + self.last_name
