181b1577b3faa20ea6e94ca0906f13acfd455b5b
רז מור <razmor5@gmail.com>
2021-03-30 23:09:02 +0300

    pass all user fields after login

--- accounts/views.py
+++ accounts/views.py
@@ -12,13 +12,17 @@ def login(request):
         username = request.POST['UserName']
         password = request.POST['Password']
 
-        for user in Account.objects.values('username', 'password'):
+        for user in Account.objects.values():
             if user['username'] == username and user['password'] == password:
                 connect = True
+                break
         if not connect:
             return render(request, 'accounts/login.html', {'form':form, 'error_message':'invalid username or password'})
         else:
-            return render(request, 'accounts/success.html', {'username': username})
+            if user['role'] == 'teacher':
+                return render(request, 'accounts/teacher_dashboard.html', user)
+            elif user['role'] == 'student':
+                return render(request, 'accounts/student_dashboard.html', user)
         
     return render(request, 'accounts/login.html', {'form':form, 'error_message': ''})
     
