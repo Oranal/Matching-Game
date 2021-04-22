d0e264d86282b245bac857ef8070f968e14fbb60
רז מור <razmor5@gmail.com>
2021-04-22 14:37:06 +0300

    BSPM2021T13-18 create a new app for the cards and the boards, build card ui, build board ui

--- matching_game/urls.py
+++ matching_game/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.conf.urls import url, include
-from accounts import urls, views
+from accounts import urls, views as accounts_views
+from game import urls, views
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('accounts.urls')),
-    url(r'^$',views.login, name = 'login'),
+    url(r'^game/', include('game.urls')),
+    url(r'^$',accounts_views.login, name = 'login'),
 ]
---
+++ static/js/game.js
@@ -0,0 +1,78 @@
+
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+let popalert = false;
+let resault;
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+
+  this.classList.add('flip');
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    resault = this;
+
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
+}
+
+function checkForMatch() {
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+    popalert = true;
+
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+
+  setTimeout(() => {
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+    
+    resetBoard();
+}, 1500);
+}
+
+function resetBoard() {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+    setTimeout(() => {
+        if (popalert){
+            popalert = false;
+            swal({
+                title: resault.dataset.framework,
+                text: "כל הכבוד, מצאת זוג",
+                icon: "success",
+                button: "יש",
+              });
+            // alert(resault.dataset.framework);
+        }
+        
+    }, 800);
+    // alert('firstCard.framework');
+}
+
+(function shuffle() {
+  cards.forEach(card => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
