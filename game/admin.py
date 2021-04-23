33926f71d5debea05d6c1d8fb5173528721f763f
רז מור <razmor5@gmail.com>
2021-04-23 14:26:42 +0300

    BSPM2021T13-18 database Institution table migrated

--- game/admin.py
+++ game/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from game.models import Board
+from game.models import Board, Institution
 
 admin.site.register(Board)
+admin.site.register(Institution)
 # Register your models here.
