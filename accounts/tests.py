4e2dae9f7df30ec61c1becccea318d7b48084a61
noamguetta97 <noamguetta97@gmail.com>
2021-05-02 15:05:32 +0300

    start to added test case for login view

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,11 +1,14 @@
-from django.test import TestCase 
+from django.test import TestCase
 from django.test import tag
 from django.urls import resolve
 from django.urls import reverse
 from accounts.models import Account
 from accounts.apps import AccontsConfig
 from accounts.views import *
-from accounts.forms import FormLogin,KindergardenForm,ChildForm
+from accounts.forms import FormLogin, KindergardenForm, ChildForm, ChildEditForm, SingleForm, DoubleForm
+
+
+# TEST MODELS:
 
 @tag('unit-test')
 class AccountTestCase(TestCase):
@@ -18,7 +21,7 @@ class AccountTestCase(TestCase):
             institution='InstitutionTest',
             role='Kindergarden',
         )[0]
-        
+
         self.child = Account.objects.get_or_create(
             username='ChildTest',
             first_name='FirstNameChildTest',
@@ -70,195 +73,239 @@ class AccountTestCase(TestCase):
         self.assertEqual(self.admin.role, 'Administrator')
 
 
-
-
-#TEST URLS:
+# TEST URLS:
 
 @tag('unit-test')
 class AccountsUrlsTest(TestCase):
     def test_admin_dashboard_url_resolved(self):
-        #Act
+        # Act
         url = reverse('admin_dashboard')
-        #Assert
+        # Assert
         self.assertEqual(resolve(url).func, admin_dashboard)
 
     def test_games_url_resolved(self):
-        #act
+        # act
         url = reverse('games')
-        #Assert
-        self.assertEqual(resolve(url).func,games)
-
+        # Assert
+        self.assertEqual(resolve(url).func, games)
 
     def test_difficulty_url_resolved(self):
-        #Act
+        # Act
         url = reverse('difficulty')
-        #Assert
-        self.assertEqual(resolve(url).func,difficulty)
-    
+        # Assert
+        self.assertEqual(resolve(url).func, difficulty)
+
     def test_new_game_url_resolved(self):
-        #Act
+        # Act
         url = reverse('new_game')
-        #Assert
-        self.assertEqual(resolve(url).func,new_game)
+        # Assert
+        self.assertEqual(resolve(url).func, new_game)
 
     def test_game_info_url_resolved(self):
-        #Act
+        # Act
         url = reverse('game_info')
-        #Assert
-        self.assertEqual(resolve(url).func,game_info)
+        # Assert
+        self.assertEqual(resolve(url).func, game_info)
 
     def test_game_topic_url_resolved(self):
-        #Act
+        # Act
         url = reverse('game_topic')
-        #Assert
-        self.assertEqual(resolve(url).func,game_topic)
+        # Assert
+        self.assertEqual(resolve(url).func, game_topic)
 
     def test_extra_card_url_resolved(self):
-        #Act
+        # Act
         url = reverse('extra_card')
-        #Assert
-        self.assertEqual(resolve(url).func,extra_card)
+        # Assert
+        self.assertEqual(resolve(url).func, extra_card)
 
     def test_done_extra_url_resolved(self):
-        #Act
+        # Act
         url = reverse('done_extra')
-        #Assert
-        self.assertEqual(resolve(url).func,done_extra)
+        # Assert
+        self.assertEqual(resolve(url).func, done_extra)
 
     def test_login_url_resolved(self):
-        #Act
+        # Act
         url = reverse('login')
-        #Assert
-        self.assertEqual(resolve(url).func,login)
+        # Assert
+        self.assertEqual(resolve(url).func, login)
 
     def test_institutions_url_resolved(self):
-        #Act
+        # Act
         url = reverse('institutions')
-        #Assert
-        self.assertEqual(resolve(url).func,institutions)
+        # Assert
+        self.assertEqual(resolve(url).func, institutions)
 
     def test_child_url_resolved(self):
-        #Act
+        # Act
         url = reverse('child')
-        #Assert
-        self.assertEqual(resolve(url).func,child)
+        # Assert
+        self.assertEqual(resolve(url).func, child)
 
     def test_iteacher_dashboard_url_resolved(self):
-        #Act
+        # Act
         url = reverse('teacher_dashboard')
-        #Assert
-        self.assertEqual(resolve(url).func,teacher_dashboard)
+        # Assert
+        self.assertEqual(resolve(url).func, teacher_dashboard)
 
     def test_teacher_dashboard_my_class_url_resolved(self):
-        #Act
+        # Act
         url = reverse('my_class')
-        #Assert
-        self.assertEqual(resolve(url).func,my_class)
+        # Assert
+        self.assertEqual(resolve(url).func, my_class)
 
     def test_teacher_dashboard_teacher_details_url_resolved(self):
-        #Act
+        # Act
         url = reverse('teacher_details')
-        #Assert
-        self.assertEqual(resolve(url).func,teacher_details)
+        # Assert
+        self.assertEqual(resolve(url).func, teacher_details)
 
     def test_child_delete_url_resolved(self):
-        #Act
+        # Act
         url = reverse('child_delete')
-        #Assert
-        self.assertEqual(resolve(url).func,child_delete)
-   
+        # Assert
+        self.assertEqual(resolve(url).func, child_delete)
+
     def test_play_games_url_resolved(self):
-        #Act
+        # Act
         url = reverse('play_game')
-        self.assertEqual(resolve(url).func,play_game)
+        self.assertEqual(resolve(url).func, play_game)
 
     def test_score_board_url_resolved(self):
-        #Act
+        # Act
         url = reverse('score_board')
-        #Assert
-        self.assertEqual(resolve(url).func,score_board)   
+        # Assert
+        self.assertEqual(resolve(url).func, score_board)
 
     def test_search_url_resolved(self):
-        #Act
+        # Act
         url = reverse('search')
-        #Assert
-        self.assertEqual(resolve(url).func,search)   
-         
-            
+        # Assert
+        self.assertEqual(resolve(url).func, search)
 
-#TEST FORMS:
+
+# TEST FORMS:
 
 @tag('unit-test')
 class FormLoginTest(TestCase):
     def setUp(self):
-        self.form_data = {'UserName' : 'userNameTest', 'Password': 'passwordTest'}
-        self.form = FormLogin(data = self.form_data)
+        self.form_data = {'UserName': 'userNameTest',
+                          'Password': 'passwordTest'}
+        self.form = FormLogin(data=self.form_data)
 
     def test_UserName_required(self):
-        #Assert
+        # Assert
         self.assertTrue(self.form.fields['UserName'].required)
 
     def test_Password_required(self):
-        #Assert
+        # Assert
         self.assertTrue(self.form.fields['Password'].required)
+
 
 class KindergardenFormTest(TestCase):
     def setUp(self):
-        self.form_data = {'FirstName' : 'FirstNameTest', 'LastName': 'LastNameTest'
-                         , 'Institution': 'InstitutionTest'}
-        self.form = KindergardenForm(data = self.form_data)
+        self.form_data = {'FirstName': 'FirstNameTest',
+                          'LastName': 'LastNameTest', 'Institution': 'InstitutionTest'}
+        self.form = KindergardenForm(data=self.form_data)
 
     def test_FirstName_required(self):
-        #Assert
+        # Assert
         self.assertTrue(self.form.fields['FirstName'].required)
-    
+
     def test_LastName_required(self):
-        #Assert
+        # Assert
         self.assertTrue(self.form.fields['LastName'].required)
-    
+
     def test_Institution_required(self):
-        #Assert
+        # Assert
         self.assertTrue(self.form.fields['Institution'].required)
 
 
 class ChildFormTest(TestCase):
     def setUp(self):
-        self.form_data = {'FirstName' : 'FirstNameTest', 'LastName': 'LastNameTest'
-                         , 'role': 'roleTest', 'rating': 0}
-        self.form = ChildForm(data = self.form_data)
+        self.form_data = {'FirstName': 'FirstNameTest',
+                          'LastName': 'LastNameTest', 'role': 'Child', 'rating': 0}
+        self.form = ChildForm(data=self.form_data)
 
     def test_FirstName_required(self):
-        #Assert
+        # Assert
         self.assertTrue(self.form.fields['FirstName'].required)
-    
+
     def test_LastName_required(self):
-        #Assert
+        # Assert
         self.assertTrue(self.form.fields['LastName'].required)
 
+    def test_get_role_func(self):
+        self.assertEqual(self.form.get_role(), "Child")
+
+    def test_get_rating_func(self):
+        self.assertEqual(self.form.get_rating(), 0)
 
 
-        
-#TEST MODELS:
+class ChildEditFormTest(TestCase):
+    def setUp(self):
+        self.form_data = {
+            'Institution': 'InstitutionTest', 'rating': 'ratingTest'}
+        self.form = ChildEditForm(data=self.form_data)
+
+    def test_Institution_required(self):
+        # Assert
+        self.assertTrue(self.form.fields['Institution'].required)
+
+    def test_rating_required(self):
+        # Assert
+        self.assertTrue(self.form.fields['rating'].required)
 
 
+class SingleFormTest(TestCase):
+    def setUp(self):
+        self.form_data = {'data': 'dataTest'}
+        self.form = SingleForm(data=self.form_data)
 
-#TEST VIEWS:
+    def test_data_required(self):
+        # Assert
+        self.assertTrue(self.form.fields['data'].required)
 
 
+# TEST VIEWS:
+
+class LoginTest(TestCase):
+    @tag('unit-test')
+    def test_view_url_exists_at_desired_location(self):
+        # Act
+        response = self.client.get('')
+        # Assert
+        self.assertEqual(response.status_code, 200)
+
+    tag('unit-test')
+
+    def test_view_url_accessible_by_name(self):
+        # Act
+        response = self.client.get(reverse(login))
+        # Assert
+        self.assertEqual(response.status_code, 200)
+
+    @tag('unit-test')
+    def test_view_uses_correct_template(self):
+        # Act
+        response = self.client.get(reverse('login'))
+        # Assert
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'accounts/login.html')
+
+    @tag('unit-test')
+    def test_view_contains_form(self):
+        # Act
+        response = self.client.get(reverse('login'))
+        # Assert
+        self.assertIsNotNone(response.context['form'])
 
 
-
-
-
-
-#TEST APP:
+# TEST APP:
 
 @tag('unit-test')
 class ProfileAppsTestCase(TestCase):
     def test_apps_name(self):
-        #Assert
+        # Assert
         self.assertEqual(AccontsConfig.name, "acconts")
-
-
-
-
